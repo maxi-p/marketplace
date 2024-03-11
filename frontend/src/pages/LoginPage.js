d32c65eb8adcb438336708573b7c2ebb3daa24e1
ZenMaster02 <zenen2002@gmail.com>
2024-03-11 17:33:27 -0400

    testing push

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PageTitle from '../components/PageTitle';
 import Login from '../components/Login';
-
+//trying to fix heroku error
 const LoginPage = () =>
 {
     return(
