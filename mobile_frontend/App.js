89135ce4d74bf2b87ea999b9acc10b08074110d1
Griffin Z <griffin.zakow@ucf.edu>
2024-03-19 16:14:42 -0400

    Added Form Hooks to Register
    TODO: Add Validation
    TODO: Connect To Server

--- mobile_frontend/App.js
+++ mobile_frontend/App.js
@@ -5,6 +5,7 @@
 // TODO Expand Image when Pressed
 import React, {} from "react"; 
 import { 
+  Alert,
   View,
   Text,
   Keyboard,
@@ -16,10 +17,19 @@ import {
   Button,
   ScrollView,
 } from "react-native"; 
+import { useForm, Controller } from "react-hook-form";
 
 
 // Move From Here
 function RegisterComponent() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => {console.log(data)};
+
   return (
     <View style={styles.root}>
       <KeyboardAvoidingView
@@ -28,95 +38,179 @@ function RegisterComponent() {
           {/* Component Starts Here*/}
           <View>
           {/* First Name*/}
-          <RegInput 
-            label="First Name"
+          <Controller 
+            name="firstName"
+            defaultValue=""
+            rules={{ required: true, 
+            }}
+            render={({field: { onChange, onBlur, value}}) => (
+              <RegInput 
+                label="First Name"
+                error={errors.firstName}
+                errorText={errors.firstName && "This is Required"}
 
-            autoCapitalize="words"
-            textContentType="givenName"
-            autoComplete="given-name"
-            autoCorrect={false}
-            keyboardType="default"
-            returnKeyType="next"
+                autoCapitalize="words"
+                textContentType="givenName"
+                autoComplete="given-name"
+                autoCorrect={false}
+                keyboardType="default"
+                returnKeyType="next"
 
-            onSubmitEditing={(event) => { event && this.LNRef.focus()}}
-            inputRef={(r) => this.FNRef = r}
+                onSubmitEditing={(event) => { event && this.LNRef.focus()}}
+                inputRef={(r) => this.FNRef = r}
+
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            control={control}
           />
+
           {/* Last Name*/}
-          <RegInput 
-            label="Last Name"
+          <Controller 
+            name="lastName"
+            defaultValue=""
+            rules={{required: true,}}
+            render={({field: {onChange, onBlur, value}}) => (
+              <RegInput 
+                label="Last Name"
+                error={errors.lastName}
+                errorText={errors.lastName && "This is required"}
 
-            autoCapitalize="words"
-            textContentType="familyName"
-            autoComplete="family-name"
-            autoCorrect={false}
-            keyboardType="default"
-            returnKeyType="next"
+                autoCapitalize="words"
+                textContentType="familyName"
+                autoComplete="family-name"
+                autoCorrect={false}
+                keyboardType="default"
+                returnKeyType="next"
 
-            onSubmitEditing={(event) => { event && this.UNRef.focus()}}
-            inputRef={(r) => this.LNRef = r}
-            
+                onSubmitEditing={(event) => { event && this.UNRef.focus()}}
+                inputRef={(r) => this.LNRef = r}
+
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                
+              />
+            )}
+            control={control}
           />
           {/* Username*/}
-          <RegInput 
-            label="Username"
+          <Controller 
+            name="username"
+            defaultValue=""
+            rules={{required: true,}} 
+            render={({field: {onChange, onBlur, value}}) => (
+              <RegInput 
+                label="Username"
+                error={errors.username}
+                errorText={errors.username && "This is required"}
 
-            autoCapitalize="none"
-            textContentType="username"
-            autoComplete="username"
-            autoCorrect={false}
-            keyboardType="default"
-            returnKeyType="next"
+                autoCapitalize="none"
+                textContentType="username"
+                autoComplete="username"
+                autoCorrect={false}
+                keyboardType="default"
+                returnKeyType="next"
 
-            onSubmitEditing={(event) => { event && this.EMRef.focus()}}
-            inputRef={(r) => this.UNRef = r}
+                onSubmitEditing={(event) => { event && this.EMRef.focus()}}
+                inputRef={(r) => this.UNRef = r}
+
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+              />
+            )}
+            control={control}
           />
           {/* Email*/}
-          <RegInput 
-            label="Email"
+          <Controller 
+            name="email"
+            defaultValue=""
+            rules={{required: true,}} 
+            render={({field: {onChange, onBlur, value}}) => (
+              <RegInput 
+                label="Email"
+                error={errors.email}
+                errorText={errors.email && "This is required"}
 
-            autoCapitalize="none"
-            textContentType="emailAddress"
-            autoComplete="email"
-            autoCorrect={false}
-            keyboardType="email-address"
-            returnKeyType="next"
+                autoCapitalize="none"
+                textContentType="emailAddress"
+                autoComplete="email"
+                autoCorrect={false}
+                keyboardType="email-address"
+                returnKeyType="next"
 
-            onSubmitEditing={(event) => { event && this.PHRef.focus()}}
-            inputRef={(r) => this.EMRef = r}
+                onSubmitEditing={(event) => { event && this.PHRef.focus()}}
+                inputRef={(r) => this.EMRef = r}
+
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+              />
+            )}
+            control={control}
           />
           {/* Phone Number*/}
-          <RegInput 
-            label="Phone Number"
+          <Controller 
+            name="phone"
+            defaultValue=""
+            rules={{required: true,}} 
+            render={({field: {onChange, onBlur, value}}) => (
+              <RegInput 
+                label="Phone Number"
+                error={errors.phone}
+                errorText={errors.phone && "This is required"}
 
-            autoCapitalize="none"
-            textContentType="telephoneNumber"
-            autoComplete="tel"
-            autoCorrect={false}
-            keyboardType="phone-pad"
-            returnKeyType="next"
+                autoCapitalize="none"
+                textContentType="telephoneNumber"
+                autoComplete="tel"
+                autoCorrect={false}
+                keyboardType="phone-pad"
+                returnKeyType="next"
 
-          onSubmitEditing={(event) => { event && this.PWRef.focus()}}
-            inputRef={(r) => this.PHRef = r}
+              onSubmitEditing={(event) => { event && this.PWRef.focus()}}
+                inputRef={(r) => this.PHRef = r}
+
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+              />
+            )}
+            control={control}
           />
           {/* Password*/}
-          <RegInput 
-            label="Password"
+          <Controller 
+            name="pass"
+            defaultValue=""
+            rules={{required: true,}} 
+            render={({field: {onChange, onBlur, value}}) => (
+              <RegInput 
+                label="Password"
+                error={errors.pass}
+                errorText={errors.pass && "This is required"}
 
-            autoCapitalize="none"
-            textContentType="password"
-            autoComplete="new-password"
-            autoCorrect={false}
-            keyboardType="visible-password"
-            returnKeyType="done"
+                autoCapitalize="none"
+                textContentType="password"
+                autoComplete="new-password"
+                autoCorrect={false}
+                keyboardType="visible-password"
+                returnKeyType="done"
 
-
-            inputRef={(r) => this.PWRef = r}
+                inputRef={(r) => this.PWRef = r}
+                
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+              />
+            )}
+            control={control}
           />
           <View style={[styles.form, styles.formButton]}>
             <Button 
-              onPress={null}
+              onPress={handleSubmit(onSubmit)}
               title="Submit"
-              disabled
             />
           </View>
         </View>
@@ -129,6 +223,7 @@ function RegisterComponent() {
   // Props:
   //  All Textinput Props
   //  label - lable text
+  //  error
   //  errorText - Error Text
   //  style effects Textinput Style
   //  formStyle
