c01a9d5f067fda4d3a7e84c09b7251a4669c0d2a
Maksim Petrushin <petrushin.maksim001@gmail.com>
2024-04-05 21:51:37 -0400

    switched register and login to useState declarative variables

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -4,67 +4,53 @@ import validateRegister from '../logic/validator';
 function buildPath(route)
 {
     const app_name = 'cop4331-marketplace-98e1376d9db6'
-    if (process.env.NODE_ENV === 'production')
+    // if (process.env.NODE_ENV === 'production')
         return 'https://' + app_name + '.herokuapp.com/' + route;
-    else
-        return 'http://localhost:5000/'+ route;
 }
 
 function Register()
 {
-    var registerFName;
-    var registerLName;
-    var registerUsername;
-    var registerPassword;
-    var registerEmail;
-    var registerPhoneNo;
+    const [formData, setFormData] = useState(
+        {firstname: "", lastname: "", username: "", password: "", email: "", phoneNumber: ""}
+    );
+
+    const handleChange = (event) =>{
+        setFormData(prevFormData => {
+            setValidationState(validateRegister(
+                {...prevFormData,
+                [event.target.id]: event.target.value
+                }
+            ));
+            
+            return {
+                ...prevFormData,
+                [event.target.id]: event.target.value
+            }
+        });
+        
+
+    };
 
     const [message,setMessage] = useState('');
-    const [firstNameResult,setFirstNameResult] = useState('');
-    const [lastNameResult,setLastNameResult] = useState('');
-    const [emailResult,setEmailResult] = useState('');
-    const [phoneResult,setPhoneResult] = useState('');
-    const [passwordResult,setPasswordResult] = useState('');
-    const [userNameResult,setUserNameResult] = useState('');
+    const [validationState, setValidationState] = useState(
+            validateRegister(formData))
 
     const doRegister = async event =>
     {
         event.preventDefault();
-        var obj = {
-            firstname: registerFName.value,
-            lastname: registerLName.value,
-            username: registerUsername.value,
-            password: registerPassword.value,
-            email: registerEmail.value,
-            phoneNumber: registerPhoneNo.value
-        };
-
         
-        const validationObject = validateRegister(obj);
-        
-        
-        if(validationObject.status === 'valid'){
-            
-            var js = JSON.stringify(obj);
+        if(validationState.status === 'valid'){
+            var json = JSON.stringify(formData);
             try
             {
-                const response = await fetch(buildPath('api/register'), {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
-                
+                const response = await fetch(buildPath('api/register'), {method:'POST',body:json,headers:{'Content-Type': 'application/json'}});
                 var res = JSON.parse(await response.text());
                 if(res.error)
                     setMessage(res.error);
                 else
                 {
-                    var user = {
-                        firstname: registerFName.value,
-                        lastname: registerLName.value,
-                        username: registerUsername.value,
-                        password: registerPassword.value,
-                        email: registerEmail.value,
-                        phoneNumber: registerPhoneNo.value
-                    };
+                    var user = {firstName:res.firstName, lastName:res.lastName, id:res.id}
                     localStorage.setItem('user_data', JSON.stringify(user));
-                    
                     setMessage('');
                     window.location.href = '/verify-email';
                 }
@@ -76,16 +62,6 @@ function Register()
                 return;
             }
         }
-        else{
-            setFirstNameResult(validationObject.firstNameMessage);
-             setLastNameResult(validationObject.lastNameMessage);
-                setEmailResult(validationObject.emailMessage);
-                setPhoneResult(validationObject.phoneMessage);
-             setPasswordResult(validationObject.passwordMessage);
-             setUserNameResult(validationObject.userNameMessage);
-        }
-
-        // alert('doRegister() ' + registerFName.value + ' ' + registerLName.value + ' ' + registerUsername.value + ' ' + registerPassword.value + ' ' + registerEmail.value + ' ' + registerPhoneNo.value);
     };
 
     const backToLogin = async event =>
@@ -99,20 +75,63 @@ function Register()
         <div id="registerDiv">
             <form onSubmit={doRegister}>
                 <span id="inner-title">PLEASE LOG IN</span><br />
-                <input type="text" id="registerFName" placeholder="First Name" ref={(c) => registerFName = c}/>
-                <span id="firstNameResult">{firstNameResult}</span><br />
-                <input type="text" id="registerLName" placeholder="Last Name" ref={(c) => registerLName = c} />
-                <span id="lastNameResult">{lastNameResult}</span><br />
-                <input type="text" id="registerEmail" placeholder="Email" ref={(c) => registerEmail = c}/>
-                <span id="emailResult">{emailResult}</span><br />
-                <input type="text" id="registerPhoneNo" placeholder="Phone number" ref={(c) => registerPhoneNo = c} />
-                <span id="phoneResult">{phoneResult}</span><br />
-                <input type="text" id="registerName" placeholder="Username" ref={(c) => registerUsername = c}/>
-                <span id="userNameResult">{userNameResult}</span><br />
-                <input type="password" id="registerPassword" placeholder="Password" ref={(c) => registerPassword = c} />
-                <span id="passwordResult">{passwordResult}</span><br />
-                <input type="submit" id="registerButton" className="buttons" value = "Register" onClick={doRegister} />
-                <input type="submit" id="backToLogin" className="buttons" value = "Back To Login" onClick={backToLogin} />
+
+                <input 
+                    type="text"
+                    id="firstname"
+                    placeholder="First Name" 
+                    onChange={handleChange}
+                />
+                <span id="firstNameResult">{validationState.firstNameMessage}</span><br />
+                <input 
+                    type="text"
+                    id="lastname"
+                    placeholder="Last Name" 
+                    onChange={handleChange} 
+                />
+                <span id="lastNameResult">{validationState.lastNameMessage}</span><br />
+                <input 
+                    type="text"
+                    id="username"
+                    placeholder="Username" 
+                    onChange={handleChange}
+                />
+                <span id="userNameResult">{validationState.userNameMessage}</span><br />
+                <input 
+                    type="password"
+                    id="password"
+                    placeholder="Password" 
+                    onChange={handleChange} 
+                />
+                <span id="passwordResult">{validationState.passwordMessage}</span><br />
+                <input 
+                    type="text"
+                    id="email"
+                    placeholder="Email" 
+                    onChange={handleChange}
+                />
+                <span id="emailResult">{validationState.emailMessage}</span><br />
+                <input 
+                    type="text"
+                    id="phoneNumber"
+                    placeholder="Phone number" 
+                    onChange={handleChange} 
+                />
+                <span id="phoneResult">{validationState.phoneMessage}</span><br /> 
+                <input 
+                    type="submit"
+                    id="registerButton"
+                    className="buttons" 
+                    value = "Register" 
+                    onClick={doRegister} 
+                />
+                <input 
+                    type="submit"
+                    id="backToLogin"
+                    className="buttons" 
+                    value="Back To Login" 
+                    onClick={backToLogin} 
+                />
             </form>
             <span id="registerResult">{message}</span>
         </div>
