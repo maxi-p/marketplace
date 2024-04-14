0e75f21913a8d2ea99230efd519b41574926bc63
ZenMaster02 <zenen2002@gmail.com>
2024-04-14 00:36:23 -0400

    login page first attempth

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
-import Route, {useNavigate} from 'react-router-dom';
+import Route, { useNavigate } from 'react-router-dom';
 import buildPath from '../logic/buildPath';
 
-const Login = props =>
-{
+const Login = props => {
     const navigate = useNavigate();
 
     const [formData, setFormData] = useState(
-        {username: "",password: ""}
+        { username: "", password: "" }
     );
-    
+
     const handleChange = (event) => {
-        const {id, value} = event.target;
+        const { id, value } = event.target;
         setFormData(prevFormData => {
             return {
                 ...prevFormData,
@@ -20,25 +19,21 @@ const Login = props =>
         })
     };
 
-    const [message,setMessage] = useState('');
+    const [message, setMessage] = useState('');
 
-    const doLogin = async (event) =>
-    {
+    const doLogin = async (event) => {
         event.preventDefault();
         var json = JSON.stringify(formData);
 
-        try
-        {
-            const response = await fetch(buildPath('api/login'), {method:'POST',body:json,headers:{'Content-Type': 'application/json'}});
+        try {
+            const response = await fetch(buildPath('api/login'), { method: 'POST', body: json, headers: { 'Content-Type': 'application/json' } });
             var res = JSON.parse(await response.text());
 
-            if( res.id <= 0 )
-            {
+            if (res.id <= 0) {
                 setMessage('User/Password combination incorrect');
             }
-            else
-            {
-                var user = {username: res.username, firstName:res.firstName, lastName:res.lastName, id:res.id}
+            else {
+                var user = { username: res.username, firstName: res.firstName, lastName: res.lastName, id: res.id }
                 localStorage.setItem('user_data', JSON.stringify(user));
                 props.loggedHandler(user);
                 setMessage('');
@@ -46,7 +41,7 @@ const Login = props =>
             }
 
         }
-        catch(e){
+        catch (e) {
             console.log(json)
             alert(e.toString());
             return;
@@ -54,46 +49,50 @@ const Login = props =>
 
     };
 
-    return(
-        <div id="loginDiv">
-            <form  className="login" onSubmit={doLogin}>
-                <span id="inner-title">PLEASE LOG IN</span><br />
-                <input 
-                    type="text"
-                    placeholder="Username"
-                    id="username"
-                    onChange={handleChange}
-                    className="userDetails" 
-                    value={formData.username}
-                />
-                <br/>
-                <input 
-                    type="password" 
-                    placeholder="Password"
-                    id="password"
-                    onChange={handleChange}
-                    className="userDetails"  
-                    value={formData.password}
-                    
-                />
-                <br/>
-                <input 
-                    type="submit" 
-                    value = "Login"
-                    id="loginButton" 
-                    className="buttons"  
-                    onClick={doLogin} 
-                />
-                <input 
-                    type="button" 
-                    value = "Register"
-                    id="registerButton"
-                    className="buttons"  
-                    onClick={() => navigate('/register')} 
-                />
-            </form>
+    return (
+        <div class="form_wrapper">
+            <div class="form_container">
+                <div class="title_container">
+                    <h2>Login</h2>
+                </div>
+
+                <div class="">
+                    <form className="login" onSubmit={doLogin}>
+                        <div class="input_field"> <span><i aria-hidden="true" class="fa fa-user"></i></span>
+                            <input
+                                type="text"
+                                placeholder="Username"
+                                id="username"
+                                onChange={handleChange}
+                                className="userDetails"
+                                value={formData.username}
+                            />
+                        </div>
+                        <div class="input_field"> <span><i aria-hidden="true" class="fa fa-lock"></i></span>
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                id="password"
+                                onChange={handleChange}
+                                className="userDetails"
+                                value={formData.password}
+
+                            />
+                        </div>
+                        <input
+                            type="submit"
+                            value="Login"
+                            id="loginButton"
+                            className="buttons"
+                            onClick={doLogin}
+                        />
+                    </form>
+                </div>
+            </div>
+
+            <p class="credit"><a href="http://www.designtheway.com" target="_blank"></a></p>
             <span id="loginResult">{message}</span>
-        </div>
+        </div >
     );
 };
 
