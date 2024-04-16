381ddd1444f0bae0f409bbabe9f4a8af4e97d81c
ZenMaster02 <zenen2002@gmail.com>
2024-04-16 00:45:46 -0400

    bringing back changes

--- frontend/src/components/PostDetails.js
+++ frontend/src/components/PostDetails.js
@@ -3,6 +3,7 @@ import buildPath from '../logic/buildPath';
 import FadeLoader from 'react-spinners/FadeLoader'
 import { DeletePost } from './DeletePost';
 import { EditPost } from './EditPost';
+import { Link } from 'react-router-dom';
 
 const PostDetails = props => {
     const id = props.id;
@@ -19,13 +20,36 @@ const PostDetails = props => {
             const json = JSON.stringify({ postId: id});
             const response = await fetch(buildPath('api/getPost'), {method:'POST',body:json,headers:{'Content-Type': 'application/json'}});
             var res = JSON.parse(await response.text());
-            setPost(res.post)
+            var newRes;
+            if (props.loggedUser)
+                newRes = {...res.post, interested: res.post.usersInterested.includes(props.loggedUser.id)};
+            else
+                newRes = res.post;
+            setPost(newRes)
             setHasUpdated(false);
             // TODO: remove this mimicking network request time (0.5 seconds)
             setTimeout(()=>{setLoading(false)}, 100)
         }
         getPost();
-    },[hasUpdated]);
+    }, [hasUpdated]);
+
+    const interestHandler = async () => {
+        console.log("userId", props.loggedUser.id, "postId", post._id)
+        if (post.interested === false) {
+            console.log("false")
+            const json = JSON.stringify({ userId: props.loggedUser.id, postId: post._id });
+            const response = await fetch(buildPath('api/interestAddition'), { method: 'POST', body: json, headers: { 'Content-Type': 'application/json' } });
+            var res = JSON.parse(await response.text());
+            console.log(res)
+        }
+        else {
+            const json = JSON.stringify({ userId: props.loggedUser.id, postId: post._id });
+            const response = await fetch(buildPath('api/interestDeletion'), { method: 'POST', body: json, headers: { 'Content-Type': 'application/json' } });
+            var res = JSON.parse(await response.text());
+            console.log(res)
+        }
+        setPost({...post, interested: !post.interested })
+    }
 
     const saveHandler = data => {
         setPost(data);
@@ -51,60 +75,71 @@ const PostDetails = props => {
 
     return (
         <div>
-            {loading? 
+            {loading ?
                 (<div className="spinner-container">
                     <FadeLoader
-                        className="spinner-loader"    
+                        className="spinner-loader"
                         color="#1a2e68"
                         size={200}
                         loading={loading}
                     />
-                </div>):
+                </div>) :
                 (<div className="post-detail-container">
                     {isDeleting &&
-                    <DeletePost 
-                        className="delete-post-popup"
-                        post={post}
-                        deleteHandler={deleteHandler}
-                    />}
-                    {isEditing && 
-                    <EditPost 
-                        className="edit-post-popup"
-                        post={post}
-                        editHandler={editHandler}
-                        saveHandler={saveHandler}
-                        setHasUpdated={updatedHandler}
-                    />}
+                        <DeletePost
+                            className="delete-post-popup"
+                            post={post}
+                            deleteHandler={deleteHandler}
+                        />}
+                    {isEditing &&
+                        <EditPost
+                            className="edit-post-popup"
+                            post={post}
+                            editHandler={editHandler}
+                            saveHandler={saveHandler}
+                            setHasUpdated={updatedHandler}
+                        />}
                     <section className='post-details'>
                         <h1>{post.name}</h1>
-                        <div className="card">
-                            {props.loggedUser && post.username === props.loggedUser.username && 
-                            <button 
-                                className="delete--badge"
-                                name="open-delete"
-                                onClick={deleteHandler}
-                            >
-                                <img src="./delete.jpg" className="card--star" />
-                            </button>}
-                            {props.loggedUser && post.username === props.loggedUser.username && 
-                            <button 
-                                className="edit--badge"
-                                name="open-edit"
-                                onClick={editHandler}
-                            >
-                                <img src="./edit.jpg" className="card--star" />
-                            </button>}
-                            <img src={post.image? "data:image/;base64,"+post.image.image.data:'./post.png'} className="card--image" />
-                            <div className="card--stats">
-                                <img src="./star.png" className="card--star" />
-                                <span className="gray"> • </span>
-                                <span className="gray">{post.name}</span>
+                        <div className="card--details">
+                            <div class="card--image--details">
+                                <img src={post.image ? "data:image/;base64," + post.image.image.data : './post.png'} />
                             </div>
-                            <p className="card--title">Author: {post.username}</p>
-                            <p className="card--title">Description: {post.desc}</p>
-                            <p className="card--title">Genre: {post.genre}</p>
-                            <p className="card--title">Condition: {post.condition}</p>
-                            <p className="card--price"><span className="bold">${post.price}</span></p>
+                            <div class="card--content">
+                                {props.loggedUser && post.username === props.loggedUser.username &&
+                                    <button
+                                        className="delete--badge"
+                                        name="open-delete"
+                                        onClick={deleteHandler}
+                                    >
+                                        <img src="./delete.jpg" className="card--star" />
+                                    </button>}
+                                {props.loggedUser &&
+                                    <button
+                                        onClick={interestHandler}
+                                        className="interestedButton"
+                                    >
+                                        <img src={post.interested ? "filled_star_p.png" : "empty_star_p.png"} className="card--star" />
+                                    </button>}
+                                {props.loggedUser && post.username === props.loggedUser.username &&
+                                    <button
+                                        className="edit--badge"
+                                        name="open-edit"
+                                        onClick={editHandler}
+                                    >
+                                        <img src="./edit.jpg" className="card--star" />
+                                    </button>}
+
+                                <div className="card--stats">
+                                    <span className="gray">{post.name}</span>
+                                </div>
+                                <Link to={"/usr="+post.username} ><p className="card--title"> Author: {post.username}</p></Link>
+                                {/* <p className="card--title">Author: {post.username}</p> */}
+                                <p className="card--title">Description: {post.desc}</p>
+                                <p className="card--title">Genre: {post.genre}</p>
+                                <p className="card--title">Condition: {post.condition}</p>
+                                <p className="card--price"><span className="bold">${post.price}</span></p>
+                            </div>
                         </div>
                     </section>
                 </div>)
