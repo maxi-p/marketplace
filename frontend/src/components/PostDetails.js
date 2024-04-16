7ce537d7a8abdbded6e9f8631210b1d2013619e1
ZenMaster02 <zenen2002@gmail.com>
2024-04-15 20:26:35 -0400

    putting interested back in post details

--- frontend/src/components/PostDetails.js
+++ frontend/src/components/PostDetails.js
@@ -14,18 +14,18 @@ const PostDetails = props => {
     console.log(post)
 
     useEffect(() => {
-        const getPost = async() => {
+        const getPost = async () => {
             setLoading(true)
-            const json = JSON.stringify({ postId: id});
-            const response = await fetch(buildPath('api/getPost'), {method:'POST',body:json,headers:{'Content-Type': 'application/json'}});
+            const json = JSON.stringify({ postId: id });
+            const response = await fetch(buildPath('api/getPost'), { method: 'POST', body: json, headers: { 'Content-Type': 'application/json' } });
             var res = JSON.parse(await response.text());
             setPost(res.post)
             setHasUpdated(false);
             // TODO: remove this mimicking network request time (0.5 seconds)
-            setTimeout(()=>{setLoading(false)}, 100)
+            setTimeout(() => { setLoading(false) }, 100)
         }
         getPost();
-    },[hasUpdated]);
+    }, [hasUpdated]);
 
     const saveHandler = data => {
         setPost(data);
@@ -36,14 +36,14 @@ const PostDetails = props => {
     }
 
     const editHandler = event => {
-        if(event === 'saved' || event.target.name === 'discard')
+        if (event === 'saved' || event.target.name === 'discard')
             setIsEditing(false)
         else
             setIsEditing(true)
     }
 
     const deleteHandler = event => {
-        if(event.target.name === 'discard')
+        if (event.target.name === 'discard')
             setIsDeleting(false);
         else
             setIsDeleting(true);
@@ -51,50 +51,57 @@ const PostDetails = props => {
 
     return (
         <div>
-            {loading? 
+            {loading ?
                 (<div className="spinner-container">
                     <FadeLoader
-                        className="spinner-loader"    
+                        className="spinner-loader"
                         color="#1a2e68"
                         size={200}
                         loading={loading}
                     />
-                </div>):
+                </div>) :
                 (<div className="post-detail-container">
                     {isDeleting &&
-                    <DeletePost 
-                        className="delete-post-popup"
-                        post={post}
-                        deleteHandler={deleteHandler}
-                    />}
-                    {isEditing && 
-                    <EditPost 
-                        className="edit-post-popup"
-                        post={post}
-                        editHandler={editHandler}
-                        saveHandler={saveHandler}
-                        setHasUpdated={updatedHandler}
-                    />}
+                        <DeletePost
+                            className="delete-post-popup"
+                            post={post}
+                            deleteHandler={deleteHandler}
+                        />}
+                    {isEditing &&
+                        <EditPost
+                            className="edit-post-popup"
+                            post={post}
+                            editHandler={editHandler}
+                            saveHandler={saveHandler}
+                            setHasUpdated={updatedHandler}
+                        />}
                     <section className='post-details'>
                         <h1>{post.name}</h1>
                         <div className="card">
-                            {props.loggedUser && post.username === props.loggedUser.username && 
-                            <button 
-                                className="delete--badge"
-                                name="open-delete"
-                                onClick={deleteHandler}
-                            >
-                                <img src="./delete.jpg" className="card--star" />
-                            </button>}
-                            {props.loggedUser && post.username === props.loggedUser.username && 
-                            <button 
-                                className="edit--badge"
-                                name="open-edit"
-                                onClick={editHandler}
-                            >
-                                <img src="./edit.jpg" className="card--star" />
-                            </button>}
-                            <img src={post.image? "data:image/;base64,"+post.image.image.data:'./post.png'} className="card--image" />
+                            {props.loggedUser && post.username === props.loggedUser.username &&
+                                <button
+                                    className="delete--badge"
+                                    name="open-delete"
+                                    onClick={deleteHandler}
+                                >
+                                    <img src="./delete.jpg" className="card--star" />
+                                </button>}
+                            {props.loggedUser && post.username === props.loggedUser.username &&
+                                <button
+                                    className="edit--badge"
+                                    name="open-edit"
+                                    onClick={editHandler}
+                                >
+                                    <img src="./edit.jpg" className="card--star" />
+                                </button>}
+                            {props.loggedUser &&
+                                <button
+                                    onClick={interestHandler}
+                                >
+                                    <img src={props.obj.interested ? "filled_star_p.png" : "empty_star_p.png"} className="card--star" />
+                                </button>}
+                            <img src={post.image ? "data:image/;base64," + post.image.image.data : './post.png'} className="card--image" />
+
                             <div className="card--stats">
                                 <img src="./star.png" className="card--star" />
                                 <span className="gray"> • </span>
